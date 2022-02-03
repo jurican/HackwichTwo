12a7666a9c1cc1a527db10f9224f28dc30b025ba
jurican <97710250+jurican@users.noreply.github.com>
2022-02-03 12:38:26 -1000

    IBAction added "Hello World"

--- HackwichTwo/ViewController.swift
+++ HackwichTwo/ViewController.swift
@@ -12,19 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // this is the code lock for viewDidLoad
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func pressMeButtonPressed(_ sender: Any) {
+        
+        print("Hello World")
     }
-    */
-
+    
+    
 }
